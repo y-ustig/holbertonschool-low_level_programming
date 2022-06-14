a3afedb733032a6bf04918fca65b55fb40c9f2ad
y-ustig <yusting16@gmail.com>
2022-06-13 18:57:21 -0700

    seven commit

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,4 +1,4 @@
-#include <studio.h>
+#include <stdio.h>
 /**
  * main - impresion
  *
@@ -6,5 +6,5 @@
  */
 int main (void)
 {printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+return (0);
 }
