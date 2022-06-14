582bf30548cab033083b93e8e5165569228a3d22
y-ustig <yusting16@gmail.com>
2022-06-13 18:53:58 -0700

    seven commit

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,2 +1,10 @@
-#!/bin/bash
-gcc -E $CFILE -o c
+#include <studio.h>
+/**
+ * main - impresion
+ *
+ * Return:retorna 0
+ */
+int main (void)
+{printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
