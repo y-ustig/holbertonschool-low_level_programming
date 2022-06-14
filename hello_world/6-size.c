54656d230ca684965dcc67dcc4a190f329b26eff
y-ustig <yusting16@gmail.com>
2022-06-13 20:18:28 -0700

    eight commit

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,2 +1,19 @@
-#!/bin/bash
-gcc -E $CFILE -o c
+#include<stdio.h>
+/**
+ * main - imprime
+ * size tamaño
+ * return 0
+ */
+int main(void) 
+{int intType;
+float floatType;
+long int longintType;
+char charType;
+long long int longlongintType;
+printf("Size of a char: %zu byte (s)\n", sizeof(charType));
+printf("Size of an int: %zu byte (s)\n", sizeof(intType));
+printf("Size of a long int: %zu byte (s)\n", sizeof(longintType));
+printf("Size of a long long int: %zu byte (s)\n", sizeof(longlongType));
+printf("Size of a float: %zu byte (s)\n", sizeof(floatType));
+return (0);
+}
