172e1db6ef22ad8fd1cbd7691f0981dec86710c9
y-ustig <yusting16@gmail.com>
2022-06-13 20:34:54 -0700

    ten commit

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,9 +1,10 @@
 #include<stdio.h>
 /**
  *main imprime lo que estamos buscando
- *size tamaño
+ *
  *return vuelve the valor zero
  */
+
 int main(void)
 {
 int intType;
@@ -11,10 +12,10 @@ float floatType;
 long int longintType;
 char charType;
 long long int longlongintType;
-printf("Size of a char: %zu byte (s)\n", sizeof(charType));
-printf("Size of an int: %zu byte (s)\n", sizeof(intType));
-printf("Size of a long int: %zu byte (s)\n", sizeof(longintType));
-printf("Size of a long long int: %zu byte (s)\n", sizeof(longlongType));
-printf("Size of a float: %zu byte (s)\n", sizeof(floatType));
+printf("Size of a char: %zu byte(s)\n", sizeof(charType));
+printf("Size of an int: %zu byte(s)\n", sizeof(intType));
+printf("Size of a long int: %zu byte(s)\n", sizeof(longintType));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(longlongType));
+printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
 return (0);
 }
