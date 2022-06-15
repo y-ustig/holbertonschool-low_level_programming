0f0a6d94ed30b195b5a96b6633b86f142621cd30
y-ustig <yusting16@gmail.com>
2022-06-14 21:45:29 -0700

    two commit

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,3 +1,20 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+/**
+ *
+ * main - print the vocabulario
+ *
+ * Return: return zero
+ */
+
+int main(void)
+{	
+char abc = 'a';
+ while(abc <= 'z')
+{
+putchar(abc);
+abc++;
+}
+return (0);
+}	
