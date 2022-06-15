f0141e22efe98d8eb928f7740a3035c5f7ae6c33
y-ustig <yusting16@gmail.com>
2022-06-15 16:12:44 -0700

    four commit

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -1,3 +1,21 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+/**
+ *main - print the vocabulario
+ *
+ *Return: return zero
+ */
+
+int main(void)
+{
+int abc = 'a';
+while (abc <= 'z')
+{
+if (!(abc == 'e' || abc == 'q'))
+putchar(abc);
+abc++;
+}
+putchar('\n');
+return (0);
+}
