92af7416159a547c550f6a19f9a023b7d491d410
y-ustig <yusting16@gmail.com>
2022-06-14 19:53:00 -0700

    one commit

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -16,11 +16,11 @@ n = rand() - RAND_MAX / 2;
 c = n % 10;
 if (c > 5)
 {
-printf("last digit of %d is %d and is greater than 5\n", n, c);
+printf("Last digit of %d is %d and is greater than 5\n", n, c);
 }
 if (c == 0)
-printf("last digit of %d is %d and is 0\n", n, c);
-if (c < 6 && c !=0)
-printf("last digit of %d is %d and is less than 6 and not 0\n", n, c);
+printf("Last digit of %d is %d and is 0\n", n, c);
+if (c < 6 && c != 0)
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, c);
 return (0);
 }
