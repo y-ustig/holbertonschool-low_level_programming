be8b8f0c3bdd22d431e3e1f2d165c3444d12493c
y-ustig <yusting16@gmail.com>
2022-06-15 21:30:00 -0700

    eight commit

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -1,3 +1,23 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+/**
+ *main - print the vocabulario
+ *
+ *Return: return zero
+ */
+
+int main(void)
+{
+int abc = 'a';
+int i;
+for (i = 48; i <= 57; i++)
+putchar (i);
+while (abc <= 'f')
+{
+putchar(abc);
+abc++;
+}
+putchar('\n');
+return (0);
+}
