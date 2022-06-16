241656bdeb154d4e98434ee855d51deda8173c30
y-ustig <yusting16@gmail.com>
2022-06-15 21:09:34 -0700

    seven commit

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -1,3 +1,19 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+/**
+ *main - print the vocabulario
+ *
+ *Return: return zero
+ */
+
+int main(void)
+{
+int i;
+for (i = 'z'; i >= 'a'; i--)
+putchar(i);
+{
+putchar('\n');
+return (0);
+}
+}
