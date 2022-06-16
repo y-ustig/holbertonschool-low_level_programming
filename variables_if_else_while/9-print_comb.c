ef9a8b559d6cea16b18cefcb937a92658537c2c7
y-ustig <yusting16@gmail.com>
2022-06-15 22:30:18 -0700

    nine commit

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -1,3 +1,24 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+/**
+ *main - print the vocabulario
+ *
+ *Return: return zero
+ */
+
+int main(void)
+{
+int i;
+for (i = 48; i <= 57; i++)
+{
+putchar(i);
+if (i < 57)
+putchar(',');
+putchar(' ');
+}
+{
+putchar('\n');
+return (0);
+}
+}
