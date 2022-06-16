3dbc5bc0c0576be3c21802f58cc4bb39667a6afe
y-ustig <yusting16@gmail.com>
2022-06-15 20:17:23 -0700

    six commit

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -1,3 +1,18 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+/**
+ *main - print the vocabulario
+ *
+ *Return: return zero
+ */
+
+int main(void)
+{
+int i;
+for (i = 48; i <= 57; i++) putchar(i);
+{
+putchar(10);
+return (0);
+}
+}
