4c627d97f42328c34738200413fc256494f40cca
y-ustig <yusting16@gmail.com>
2022-06-15 17:41:17 -0700

    five commit

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -1,3 +1,19 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+/**
+ *main - print the vocabulario
+ *
+ *Return: return zero
+ */
+
+int main(void)
+{
+{
+int i;
+for(i = 0; i < 10; i++);
+printf("%d", i++);
+}
+printf("\n");
+return (0);
+}
