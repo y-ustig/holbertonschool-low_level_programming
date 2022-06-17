2a2c90800b3ddb60f09c7b3d0027f480e47a8edc
y-ustig <yusting16@gmail.com>
2022-06-17 13:16:28 -0700

    zero commit

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *print_alphabet - print the vocabulario
+ *
+ */
+
+void print_alphabet(void)
+{
+    int abc = 'a';
+    for (abc = 'a';abc <= 'z';abc++)
+    {
+	    _putchar(abc);
+    }
+    _putchar('\n');
+}
