8bbcc1163d81d14d3eca087e8dd5121be9496469
y-ustig <yusting16@gmail.com>
2022-06-17 16:51:56 -0700

    zero commit

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -6,13 +6,13 @@
 
 void print_alphabet_x10(void)
 {
-	int new_line = 10;
-	int alphabet = 122;
-	for (new_line = 1; new_line <= 10; new_line++)
-	{
-		for (alphabet = 97; alphabet <= 122; alphabet++)
-			putchar(alphabet);
-		putchar('\n');
-	}
+int new_line = 10;
+int alphabet = 122;
+for (new_line = 1; new_line <= 10; new_line++)
+{
+for (alphabet = 97; alphabet <= 122; alphabet++)
+putchar(alphabet);
+putchar('\n');
+}
 }
 
