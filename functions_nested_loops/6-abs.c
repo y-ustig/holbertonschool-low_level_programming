ab73798aea4a86cb233380afe1dc3aee884667fd
y-ustig <yusting16@gmail.com>
2022-06-18 14:41:11 -0700

    zero commit

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- *_isalpha - print the 1 when is mayus or 0 when is lower
- *@int soul
- *Return: retorna 1 o 0 si no
+ *_abs - the function for return a absolute valor
+ *@number_absolute number absolute
+ *Return: return the valor absolute of a number
  */
 int _abs(int number_absolute)
 {
