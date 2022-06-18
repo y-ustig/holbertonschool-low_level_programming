0b96fa812efc6286d0d81b2f37f84b1b4b0e0fa1
y-ustig <yusting16@gmail.com>
2022-06-17 20:46:32 -0700

    zero commit

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,10 +1,17 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
+#include "main.h"
 /**
- *main - print the vocabulario
- *
- *Return: return zero
+ *_islower - print the 1 when is mayus or 0 when is lower
+ *@c: la cantidad
+ *Return: retorna los valores
  */
-
-int main(void)
+int _islower(int c)
+{
+if (c <= 'z' && c >= 'a')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
