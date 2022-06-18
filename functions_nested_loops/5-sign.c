2695b44de977fbaadb762d6a09ee8691c2bbda7c
y-ustig <yusting16@gmail.com>
2022-06-17 21:54:51 -0700

    zero commit

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,10 +1,24 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
+#include "main.h"
 /**
- *main - print the vocabulario
- *
- *Return: return zero
+ *print_sign - print the 1 when is mayus or 0 when is lower
+ *@n: this is variable of alphabet
+ *Return: retorna 1 o 0 si no
  */
-
-int main(void)
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+if (n == 0)
+{
+_putchar('0');
+return (0);
+}
+if (n < 0)
+{
+_putchar('-');
+}
+return (-1);
+}
