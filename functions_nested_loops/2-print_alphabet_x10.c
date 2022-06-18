4682bbc5b9020eb3be5572834e5813fd5daf7ac7
y-ustig <yusting16@gmail.com>
2022-06-17 17:06:18 -0700

    zero commit

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -11,8 +11,8 @@ int alphabet = 122;
 for (new_line = 1; new_line <= 10; new_line++)
 {
 for (alphabet = 97; alphabet <= 122; alphabet++)
-putchar(alphabet);
-putchar('\n');
+_putchar(alphabet);
+_putchar('\n');
 }
 }
 
