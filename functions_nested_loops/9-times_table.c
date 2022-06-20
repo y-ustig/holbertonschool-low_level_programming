1335591910f03f9648a4781bd3328ef24dbe224d
y-ustig <yusting16@gmail.com>
2022-06-20 15:55:53 -0700

    zero commit

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,10 +1,33 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
+#include "main.h"
 /**
- *main - print the vocabulario
+ *times_table - print the alphabet 10x
  *
- *Return: return zero
  */
 
-int main(void)
+void times_table(void)
+{
+int new_line = 0;
+int number = 9;
+char eat = ',';
+char space = ' ';
+char space_t = ' ';
+int resultado;
+for (new_line = 0; new_line <= 9; new_line++)
+{
+for (number = 0; number <= 9; number++)
+{
+resultado = number * new_line;
+if (resultado > 9)
+_putchar ((resultado / 10) + '0');
+_putchar((resultado % 10) + '0');
+if (number <= 8)
+{
+_putchar(eat);
+_putchar(space);
+}
+if (resultado <= 9 && number <= 8)
+_putchar(space_t);
+}
+_putchar('\n');
+}
+}
