ecce04cfaabb5fdce60870550fed654baa69ec4e
y-ustig <yusting16@gmail.com>
2022-06-19 17:54:39 -0700

    zero commit

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,10 +1,23 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
+#include "main.h"
 /**
- *main - print the vocabulario
+ *jack_bauer - the function return the last number
  *
- *Return: return zero
+ *Return: return the last number
  */
-
-int main(void)
+void jack_bauer(void)
+{
+int hour;
+int minutes;
+for (hour = 0; hour <= 23; hour++)
+{
+for (minutes = 0; minutes <= 59; minutes++)
+{
+_putchar((hour / 10) + '0');
+_putchar((hour % 10) + '0');
+_putchar(':');
+_putchar((minutes / 10) + '0');
+_putchar((minutes % 10) + '0');
+_putchar(' ');
+}
+}
+}
