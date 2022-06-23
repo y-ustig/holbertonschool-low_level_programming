c8db2f4ceb2424d178b67e67e70e85391ab91938
y-ustig <yusting16@gmail.com>
2022-06-22 19:56:38 -0700

    zero commit

---
+++ debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+*positive_or_negative - tests function that prints if integer
+*Return: 0
+*/
+
+int positive_or_negative(void)
+{
+int i;
+i = 0;
+positive_or_negative(i);
+return (0);
+}
