1ba0358be49a15c461cfa2e0576a00063787f22b
y-ustig <yusting16@gmail.com>
2022-06-23 18:28:00 -0700

    zero commit

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *_isdigit - print the 1 when is mayus or 0 when is lower
+ *@c: la cantidad
+ *Return: retorna los valores
+ */
+int _isdigit(int c)
+{
+if (c <= 0 && c >= 9)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
