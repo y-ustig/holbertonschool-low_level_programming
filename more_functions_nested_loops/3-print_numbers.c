35fc4923c3f72b3374d750fc205994e5e38eae74
y-ustig <yusting16@gmail.com>
2022-06-24 12:55:21 -0700

    zero commit

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - function for print numbers
+ * @numbers: numbers
+ * Return - the valor
+ */
+
+void print_numbers(void)
+{
+int numbers = 48;
+for (numbers = 48; numbers <= 57; numbers++)
+{
+_putchar(numbers);
+}
+_putchar('\n');
+}
