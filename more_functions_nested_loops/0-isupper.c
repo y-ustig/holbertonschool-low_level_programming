cf7babef8c8ed5035537b01ef9cb2e0cf084a524
y-ustig <yusting16@gmail.com>
2022-06-23 17:50:03 -0700

    zero commit

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *_islower - print the 1 when is mayus or 0 when is lower
+ *@c: la cantidad
+ *Return: retorna los valores
+ */
+int _isupper(int c)
+{
+if (c <= 'Z' && c >= 'A')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
---
+++ more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+more funtions de Cfor the logical program
