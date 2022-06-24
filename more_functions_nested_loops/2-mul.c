9613d5c8ec8a7df503c0d5c53d8aee6fec8dff24
y-ustig <yusting16@gmail.com>
2022-06-24 09:05:35 -0700

    zero commit

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *mul - functions for multiplicar more numbers
+ *@b: the number z
+ *@a: the number x
+ *@resultado: producto
+ *Return: retorna los valores
+ */
+int mul(int a, int b)
+{
+int resultado = a * b;
+{
+printf("%d", resultado);
+}
+return (0);
+}
