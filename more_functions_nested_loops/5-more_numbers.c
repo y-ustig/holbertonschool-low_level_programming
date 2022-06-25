34569a90fc3b82a14ef58393ece3ea3574ced213
y-ustig <yusting16@gmail.com>
2022-06-24 19:40:40 -0700

    zero commit

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -5,15 +5,15 @@
 
 void more_numbers(void)
 {
-	int a, b;
-	for (a = 0; a <= 9; a++)
-	{
-		for (b = 0; b < 15; b++)
-		{
-			if (b > 9)
-				_putchar(( '0' + b / 10));
-			_putchar(('0' + b % 10));
-		}
-		_putchar('\n');
-	}
+int a, b;
+for (a = 0; a <= 9; a++)
+{
+for (b = 0; b < 15; b++)
+{
+if (b > 9)
+_putchar(('0' + b / 10));
+_putchar(('0' + b % 10));
+}
+_putchar('\n');
+}
 }
