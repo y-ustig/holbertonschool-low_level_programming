3daf943b32b626ee3d2d2f6a46f8cfdc9757be74
y-ustig <yusting16@gmail.com>
2022-06-24 19:31:50 -0700

    zero commit

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *more_numbers - function for print numbers
+ */
+
+void more_numbers(void)
+{
+	int a, b;
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b < 15; b++)
+		{
+			if (b > 9)
+				_putchar(( '0' + b / 10));
+			_putchar(('0' + b % 10));
+		}
+		_putchar('\n');
+	}
+}
