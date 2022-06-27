6f35831a08564f6c155c385ae030dbc9b33e7538
y-ustig <yusting16@gmail.com>
2022-06-27 15:51:39 -0700

    zero commit

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - contiene todo
+ *
+ * Return: zero l¿always
+ */
+
+int main(void)
+{
+	int j;
+
+	for (j = 1; j <= 100; j++)
+
+	{
+		if (j % 3 == 0 && j % 5 == 0)
+			printf("FizzBuzz ");
+		else if (j % 5 == 0)
+			printf("Buzz ");
+		else if (j % 3 == 0)
+			printf("Fizz ");
+		else
+			printf("%d ", j);
+	}
+	printf("\n");
+	return (0);
+}
