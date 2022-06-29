35239355ac04995c55239ac7cb39b0ea0ee2ca11
y-ustig <yusting16@gmail.com>
2022-06-29 08:53:51 -0700

    zero commit

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *_puts - print the serie characteres and followed a new line in the stdout
+ *@str: the string of characteres
+ */
+void _puts(char *str)
+{
+
+	puts(str);
+}
