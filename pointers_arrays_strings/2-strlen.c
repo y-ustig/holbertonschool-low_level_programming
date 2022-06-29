a26f9056a4d577ccdc2642cf442621f4079b08e3
y-ustig <yusting16@gmail.com>
2022-06-28 21:33:16 -0700

    zero commit

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+/**
+ *_strlen - for print  the characteres
+ *@s: it is a pointers
+ *Return: return the value of quanty characteres
+ */
+
+int _strlen(char *s)
+{
+	int you_can;
+
+	you_can = strlen(s);
+		return (you_can);
+
+}
